426c3590eb8c591445eeb6f17c965ac1d093f39f
Dmitry Belousov <dmitry1998.ua@outlook.com>
2018-11-20 16:31:17 +0200

    elasticsearch and redis cache

---
+++ app/Services/Redis/Cache.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 20.11.18
+ * Time: 15:46
+ */
+
+namespace App\Services\Redis;
+
+
+class Cache
+{
+
+}
---
+++ app/Services/Searcher/Elastic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitry
+ * Date: 20.11.18
+ * Time: 14:45
+ */
+
+namespace App\Services\Searcher;
+
+
+class Elastic
+{
+
+}
