f9528474f5c8e02ccf76b7220c2b9594478251e2
Dmitry Belousov <dmitry1998.ua@outlook.com>
2018-11-25 10:22:10 +0200

    fix localization

--- routes/web.php
+++ routes/web.php
@@ -45,22 +45,7 @@ Route::group(['prefix' => App\Http\Middleware\LocaleMiddleware::getLocale()], fu
 
 });
 
-Route::get('setlocale/{lang}', function ($lang) {
-
-    $referer = Redirect::back()->getTargetUrl();;
-    $parse_url = parse_url($referer, PHP_URL_PATH);
-    $segments = explode('/', $parse_url);
-    if (in_array($segments[1], App\Http\Middleware\LocaleMiddleware::$languages)) {
-        unset($segments[1]);
-    }
-    array_splice($segments, 1, 0, $lang);
-    $url = Request::root().implode("/", $segments);
-    if(parse_url($referer, PHP_URL_QUERY)){
-        $url = $url.'?'. parse_url($referer, PHP_URL_QUERY);
-    }
-    return redirect($url);
-
-})->name('setlocale');
+Route::get('setlocale/{lang}', 'Front\LocaleController@index')->name('setlocale');
 
 
 
